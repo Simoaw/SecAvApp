7209d65f04899165bf85f0f34b7a77fd5ae6b8ff
Simoaw <76632645+Simoaw@users.noreply.github.com>
2021-01-11 17:16:34 +0100

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -2,9 +2,9 @@
 session_start();
 // Change this to your connection info.
 $DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_USER = 'webapp';
+$DATABASE_PASS = 'root';
+$DATABASE_NAME = 'webappdb';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
